682c1c3fba8f9d615cd6fbb7962e88a52d721fa5
Annemarii Hunt <annemarii.hunt@gmail.com>
2019-06-01 14:01:17 +0300

    Üritasin lisada sündmuse kustutamist

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -29,16 +29,19 @@ export class HomePage implements OnInit {
   viewTitle = "";
   @ViewChild(CalendarComponent) myCal: CalendarComponent;
 
-  constructor(private alertCtrl: AlertController, @Inject(LOCALE_ID) private locale: string) { }
- 
+  constructor(
+    private alertCtrl: AlertController,
+    @Inject(LOCALE_ID) private locale: string
+  ) {}
+
   ngOnInit() {
     this.resetEvent();
   }
 
   resetEvent() {
     this.event = {
-      title: '',
-      desc: '',
+      title: "",
+      desc: "",
       startTime: new Date().toISOString(),
       endTime: new Date().toISOString(),
       allDay: false
@@ -51,74 +54,103 @@ export class HomePage implements OnInit {
       startTime: new Date(this.event.startTime),
       endTime: new Date(this.event.endTime),
       allDay: this.event.allDay
-    }
+    };
 
     if (eventCopy.allDay) {
       let start = eventCopy.startTime;
       let end = eventCopy.endTime;
- 
-      eventCopy.startTime = new Date(Date.UTC(start.getUTCFullYear(), start.getUTCMonth(), start.getUTCDate()));
-      eventCopy.endTime = new Date(Date.UTC(end.getUTCFullYear(), end.getUTCMonth(), end.getUTCDate() + 1));
+
+      eventCopy.startTime = new Date(
+        Date.UTC(
+          start.getUTCFullYear(),
+          start.getUTCMonth(),
+          start.getUTCDate()
+        )
+      );
+      eventCopy.endTime = new Date(
+        Date.UTC(end.getUTCFullYear(), end.getUTCMonth(), end.getUTCDate() + 1)
+      );
     }
- 
+
     this.eventSource.push(eventCopy);
     this.myCal.loadEvents();
     this.resetEvent();
-  
+  }
 
+  editEvent() {}
+
+  removeEvent() {
+    // text: 'Delete';
+    // handler: () => {
+
+    //   for (var i = 0; i <= this.eventSource.length; i++) {
+    //     if (this.eventSource[i] ) {
+    //      this.eventSource.splice(i)
+         
+         
+    //     }
+        
+    //   }
+    //   //this.eventSource.splice(event)
+    //   console.log('delete clicked');
+    // }
+
+
+    // splice() the array of events
   }
 
   onCurrentDateChanged() {}
   reloadSource() {}
 
- // Change current month/week/day
- next() {
-  var swiper = document.querySelector('.swiper-container')['swiper'];
-  swiper.slideNext();
-}
- 
-back() {
-  var swiper = document.querySelector('.swiper-container')['swiper'];
-  swiper.slidePrev();
-}
- 
-// Change between month/week/day
-changeMode(mode) {
-  this.calendar.mode = mode;
-}
- 
-// Focus today
-today() {
-  this.calendar.currentDate = new Date();
-}
- 
-// Selected date reange and hence title changed
-onViewTitleChanged(title) {
-  this.viewTitle = title;
-}
- 
-// Calendar event was clicked
-async onEventSelected(event) {
-  // Use Angular date pipe for conversion
-  let start = formatDate(event.startTime, 'medium', this.locale);
-  let end = formatDate(event.endTime, 'medium', this.locale);
- 
-  const alert = await this.alertCtrl.create({
-    header: event.title,
-    subHeader: event.desc,
-    message: 'From: ' + start + '<br><br>To: ' + end,
-    buttons: ['OK']
-  });
-  alert.present();
-}
- 
-// Time slot was clicked
-onTimeSelected(ev) {
-  let selected = new Date(ev.selectedTime);
-  this.event.startTime = selected.toISOString();
-  selected.setHours(selected.getHours() + 1);
-  this.event.endTime = (selected.toISOString());
-}
+  // Change current month/week/day
+  next() {
+    var swiper = document.querySelector(".swiper-container")["swiper"];
+    swiper.slideNext();
+  }
 
+  back() {
+    var swiper = document.querySelector(".swiper-container")["swiper"];
+    swiper.slidePrev();
+  }
 
+  // Change between month/week/day
+  changeMode(mode) {
+    this.calendar.mode = mode;
+  }
+
+  // Focus today
+  today() {
+    this.calendar.currentDate = new Date();
+  }
+
+  // Selected date reange and hence title changed
+  onViewTitleChanged(title) {
+    this.viewTitle = title;
+  }
+
+  // Calendar event was clicked
+  async onEventSelected(event) {
+    // Use Angular date pipe for conversion
+    let start = formatDate(event.startTime, "medium", this.locale);
+    let end = formatDate(event.endTime, "medium", this.locale);
+
+    const alert = await this.alertCtrl.create({
+      header: event.title,
+      subHeader: event.desc,
+      message: "From: " + start + "<br><br>To: " + end,
+      buttons: [{
+				text: "Delete",
+				action: "removeEvent"
+			},"OK"]
+    });
+    alert.present();
+  }
+
+  // Time slot was clicked
+  onTimeSelected(ev) {
+    let selected = new Date(ev.selectedTime);
+    this.event.startTime = selected.toISOString();
+    selected.setHours(selected.getHours() + 1);
+    this.event.endTime = selected.toISOString();
+  }
 }
